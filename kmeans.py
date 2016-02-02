0ac102709440e220d0c92b9223c3735c8adebea0
Shalin Luo <luoshalin@LuoShalins-MBP.wv.cc.cmu.edu>
2016-02-01 22:31:27 -0500

    work correctly - kmeans++ added

--- kmeans.py
+++ kmeans.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import math
+import numpy as np
 
 # input format: python kmeans.py [cluster_num]
 
@@ -21,20 +22,30 @@ def main(argv):
     # get total cluster number from input
     cluster_num = int(sys.argv[1])
 
+    # # ============================================ #
     # # METHOD#1: generate centroid indexes randomly
     # centroid_index_list = random.sample(list(range(0, len(vector_list)-1)), cluster_num)    # a list of centroids' indexes
     # # generate the centroid list
     # centroid_list = list()
     # for centroid_index in centroid_index_list:
     #     centroid_list.append(vector_list[centroid_index])
+    # # ============================================ #
 
+
+    # ============================================ #
     # METHOD#2: generate centroids indexes by kmeans++
     centroid_list = kpp(vector_list, cluster_num)
-    print centroid_list
+    # ============================================ #
 
-    iter_time = 10
+    # print centroid_list
+
     clusters = dict()  # save {class1: vectorList1; class2: vectorList2; ...}
-    for iter_i in range(0, iter_time):  # [0, iter_time-1]
+    stop = False
+    # for iter_i in range(0, iter_time):  # [0, iter_time-1]
+    itr = 0
+    while not stop:
+        # print "CURRENT ITERATION: " + str(itr)
+        itr += 1
         # cluster each vector
         clusters.clear()
         for v_index, vector in enumerate(vector_list):
@@ -58,6 +69,7 @@ def main(argv):
         # print clusters
 
         # recalculate the center for each cluster
+        old_centroid_list = list(centroid_list)
         for key, cluster in clusters.iteritems():   # key is the cluster index, cluster is a list of vector_index
             # update the center for current cluster
             v1 = vector_list[cluster[0]]
@@ -88,7 +100,17 @@ def main(argv):
                 index += 1
             v1 = [(item[0], float(item[1])/float((len(cluster)))) for item in v1]  # new centroid - use float
             centroid_list[key] = v1
+        # stop iteration judgement: compare old centroid list & current centroid list
+        converge = True
+        for cur, old in zip(centroid_list, old_centroid_list):
+            if (1 - get_dis(cur, old)) > 10e-3:  # 1 means the 2 centroids are the same
+                converge = False
+                break
+        if converge:
+            stop = True
 
+
+    print "TOTAL ITERATION TIME: " + str(itr)
     # output the res
     for key, cluster in clusters.iteritems():   # key is the cluster index, cluster is a list of vector_index
         for index in cluster:
@@ -137,35 +159,34 @@ def normalize(v):
 
 # the function to generate initial centroids by kmeans++
 def kpp(vector_list, cluster_num):
+    vector_list_len = len(vector_list)
     centroid_index_list = []
     centroid_list = []
+    sim_list = [sys.float_info.min] * vector_list_len   # save all vectors' similarities
 
     # 1. select the first centroid randomly
-    first_cindex = random.sample(list(range(0, len(vector_list)-1)), 1)[0]
+    first_cindex = random.sample(list(range(0, vector_list_len-1)), 1)[0]
     centroid_index_list.append(first_cindex)
     centroid_list.append(vector_list[first_cindex])
 
     # get all cluster_num centroids
     for i in range(1, cluster_num):
-        min_max_sim = sys.float_info.max
-        next_cindex = -1
-        # 2. for each vector in vector_list, calculate the dis with its nearest centroid (max similarity)
+        # calculate the sim of each vector & the last centroid in the list
+        centroid = centroid_list[-1]
         for v_index, vector in enumerate(vector_list):
-            if v_index in centroid_index_list:  # skip the already chosen centroids
-                continue
-            max_sim = sys.float_info.min
-
-            # find current vector's max-similarity, maintain the vector's index & max_similarity
-            for centroid in centroid_list:
-                cur_dis = get_dis(vector, centroid)
-                if cur_dis > max_sim:
-                    max_sim = cur_dis
-            # NOW: get the max_sim for current vector
-            if min_max_sim > max_sim:
-                next_cindex = v_index
+            cur_sim = get_dis(vector, centroid)
+            sim_list[v_index] = max(cur_sim, sim_list[v_index])
+        # NOW: sim_list[i] is vector_list[i]'s max sim with all current centroids
+        # do sampling & generate next centroid
+        sam_list = map(lambda x: (1-x)**2, sim_list)
+        sam_sum = sum(sam_list)
+        # normalize
+        norm_sam_list = map(lambda x: float(x)/sam_sum, sam_list)
+        next_cindex = np.random.choice(vector_list_len, 1, p=norm_sam_list)
         centroid_index_list.append(next_cindex)
         centroid_list.append(vector_list[next_cindex])
     return centroid_list
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
